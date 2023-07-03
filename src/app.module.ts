c41681761fea9961bf542fbe2dffe72c1aae3186
taacc <ricardotangone@gmail.com>
2023-07-03 23:52:11 +0800

    feat: add content module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
+import { ContentModule } from './modules/content/content.module';
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ContentModule],
   controllers: [AppController],
   providers: [AppService],
 })
